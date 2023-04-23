26435783f10ca59bd814f9b59825548234ecf1d9
Karaso Eran <karasoe@my.hit.ac.il>
2023-04-23 17:12:20 +0300

    updated to print as the requested result. saves this.degree only if the other degree is larger than this degree.

--- OOP_Project/Polynomial.cpp
+++ OOP_Project/Polynomial.cpp
@@ -43,21 +43,21 @@ Polynomial& Polynomial::operator=(const Polynomial& other)
         return *this;
     }
 
-    /*if (other._degree > this->_degree) {
+    if (other._degree > this->_degree) {
         delete[] _coeffs;
         this->_coeffs = new double[other._degree + 1]();
+        this->_degree = other._degree;
     }
     else
     {
         for (int i = 0; i <= _degree; i++)
             this->_coeffs[i] = 0;
-    }*/
-    this->_coeffs = new double[other._degree + 1]();
-    this->_degree = other._degree;
+    }
+    //this->_coeffs = new double[other._degree + 1]();
 
-    for (int i = 0; i <= _degree; i++)
+    for (int i = 0; i <= other._degree; i++)
     {
-        this->_coeffs[i] = other.getCoeff(i);
+        this->_coeffs[i] = other._coeffs[i];
     }
 
     updateMaxDegree();
@@ -84,12 +84,12 @@ int Polynomial::getDegree(bool iterate) const
 
 double Polynomial::getCoeff(int index) const
 {
-    //if (_coeffs == NULL || !(index >= 0 && index <= _degree))
-    //    return -1234.12;
-    if (index < 0)
+    if (_coeffs == NULL || !(index >= 0 && index <= _degree))
+        return -1234.12;
+    /*if (index < 0)
         return -1234.12;
     if (index > _degree)
-        return 0;
+        return 0;*/
 
     return _coeffs[index];
 }
